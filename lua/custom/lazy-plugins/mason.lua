0ecff0afd08747dc07f43deab77284051a2f6f5a
taaacse4 <selim.acerbas@swisscom.com>
2025-07-17 19:06:03 +0200

    update mason

--- lua/custom/lazy-plugins/mason.lua
+++ lua/custom/lazy-plugins/mason.lua
@@ -51,6 +51,46 @@ return {
             })
 
             ---------------------
+            -- LSP Setup (one handler)
+            ---------------------
+            local lspconfig   = require("lspconfig")
+            local schemastore = require("schemastore")
+            local caps        = require("cmp_nvim_lsp").default_capabilities()
+
+            require("mason-lspconfig").setup_handlers({
+                function(server_name) -- default for all servers
+                    local opts = { capabilities = caps }
+
+                    if server_name == "yamlls" then
+                        opts.filetypes = { "yaml", "yml", "Kptfile", "yaml.helm-values" }
+                        opts.on_attach = function(client, bufnr)
+                            -- turn on the LS’s formatter
+                            client.server_capabilities.documentFormattingProvider = true
+                            -- your existing on_attach (if any)
+                        end
+                        opts.settings = {
+                            yaml = {
+                                format      = { enable = true },
+                                schemaStore = { enable = true },
+                                validate    = true,
+                                hover       = true,
+                                completion  = true,
+                                schemas     = vim.tbl_extend("force",
+                                    schemastore.yaml.schemas(), {
+                                        ["https://raw.githubusercontent.com/GoogleContainerTools/kpt/main/site/reference/schema/kptfile/kptfile.yaml"] = "Kptfile",
+                                        ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
+                                    }
+                                ),
+                            },
+                            redhat = { telemetry = { enabled = false } },
+                        }
+                    end
+
+                    lspconfig[server_name].setup(opts)
+                end,
+            })
+
+            ---------------------
             -- Formatting: Conform
             ---------------------
             require("conform").setup({
@@ -113,54 +153,54 @@ return {
             ---------------------
             -- LSP Setup
             ---------------------
-            local lspconfig = require('lspconfig')
-            local schemastore = require('schemastore')
-
-            lspconfig.rust_analyzer.setup {
-                settings = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = { command = "clippy" },
-                        diagnostics = { enable = true },
-                        cargo = { allFeatures = true },
-                        rustfmt = { enable = true },
-                    },
-                },
-            }
-
-            lspconfig.jsonls.setup {
-                settings = {
-                    json = {
-                        schemas = schemastore.json.schemas(),
-                        validate = { enable = true },
-                    },
-                },
-            }
-
-            lspconfig.yamlls.setup {
-                filetypes = { "Kptfile", "yaml", "yml" },
-                settings = {
-                    yaml = {
-                        schemas = vim.tbl_extend("force", schemastore.yaml.schemas(), {
-                            ["https://raw.githubusercontent.com/GoogleContainerTools/kpt/main/site/reference/schema/kptfile/kptfile.yaml"] = "Kptfile",
-                            ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
-                        }),
-                        validate = true,
-                        hover = true,
-                        completion = true,
-                    },
-                },
-            }
-
-            -- Other LSPs
-            lspconfig.bzl.setup { filetypes = { "star" } }
-            lspconfig.texlab.setup {}
-            lspconfig.clangd.setup {}
-            lspconfig.dockerls.setup {}
-            lspconfig.bashls.setup {}
-            lspconfig.helm_ls.setup {}
-            lspconfig.html.setup {}
-            lspconfig.lemminx.setup {}
-            lspconfig.cmake.setup {}
+            -- local lspconfig = require('lspconfig')
+            -- local schemastore = require('schemastore')
+            --
+            -- lspconfig.rust_analyzer.setup {
+            --     settings = {
+            --         ["rust-analyzer"] = {
+            --             checkOnSave = { command = "clippy" },
+            --             diagnostics = { enable = true },
+            --             cargo = { allFeatures = true },
+            --             rustfmt = { enable = true },
+            --         },
+            --     },
+            -- }
+            --
+            -- lspconfig.jsonls.setup {
+            --     settings = {
+            --         json = {
+            --             schemas = schemastore.json.schemas(),
+            --             validate = { enable = true },
+            --         },
+            --     },
+            -- }
+            --
+            -- lspconfig.yamlls.setup {
+            --     filetypes = { "Kptfile", "yaml", "yml" },
+            --     settings = {
+            --         yaml = {
+            --             schemas = vim.tbl_extend("force", schemastore.yaml.schemas(), {
+            --                 ["https://raw.githubusercontent.com/GoogleContainerTools/kpt/main/site/reference/schema/kptfile/kptfile.yaml"] = "Kptfile",
+            --                 ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
+            --             }),
+            --             validate = true,
+            --             hover = true,
+            --             completion = true,
+            --         },
+            --     },
+            -- }
+            --
+            -- -- Other LSPs
+            -- lspconfig.bzl.setup { filetypes = { "star" } }
+            -- lspconfig.texlab.setup {}
+            -- lspconfig.clangd.setup {}
+            -- lspconfig.dockerls.setup {}
+            -- lspconfig.bashls.setup {}
+            -- lspconfig.helm_ls.setup {}
+            -- lspconfig.html.setup {}
+            -- lspconfig.lemminx.setup {}
+            -- lspconfig.cmake.setup {}
         end,
     },
 }
