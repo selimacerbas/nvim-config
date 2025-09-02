b881361f46b5271a8ef5f50567f50dbbc1fbe96b
taaacse4 <selim.acerbas@swisscom.com>
2025-09-02 19:12:25 +0200

    update git signs

--- lua/custom/lazy-plugins/git-signs.lua
+++ lua/custom/lazy-plugins/git-signs.lua
@@ -1,44 +1,68 @@
 return {
     {
-        'lewis6991/gitsigns.nvim',
-        dependencies = { 'folke/which-key.nvim' },
-        config = function()
-            local which_key = require('which-key')
-            local gitsigns  = require('gitsigns')
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "folke/which-key.nvim" },
+        opts = {
+            signs = {
+                add          = { text = "+" },
+                change       = { text = "┃" },
+                delete       = { text = "_" },
+                topdelete    = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+            current_line_blame = true,
+            current_line_blame_opts = { delay = 400, virt_text_pos = "eol" },
+            preview_config = { border = "single" },
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
 
-            gitsigns.setup({
-                signs = {
-                    add          = { text = '+' },
-                    change       = { text = '┃' },
-                    delete       = { text = '_' },
-                    topdelete    = { text = '‾' },
-                    changedelete = { text = '~' },
-                },
-                current_line_blame = true,
-                on_attach = function(bufnr)
-                    -- leave these global so they work in any buffer:
-                    vim.keymap.set('n', ']c', gitsigns.next_hunk, { desc = 'Next Git Hunk', silent = true })
-                    vim.keymap.set('n', '[c', gitsigns.prev_hunk, { desc = 'Prev Git Hunk', silent = true })
+                local function map(mode, lhs, rhs, desc)
+                    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+                end
 
-                    -- all other actions under <leader>h via which-key
-                    which_key.register({
-                        g = {
-                            name = "Git",
-                            s = { '<cmd>Gitsigns stage_hunk<CR>', 'Stage Hunk' },
-                            r = { '<cmd>Gitsigns reset_hunk<CR>', 'Reset Hunk' },
-                            S = { gitsigns.stage_buffer, 'Stage Buffer' },
-                            u = { gitsigns.undo_stage_hunk, 'Undo Stage Hunk' },
-                            R = { gitsigns.reset_buffer, 'Reset Buffer' },
-                            p = { gitsigns.preview_hunk, 'Preview Hunk' },
-                            b = { gitsigns.blame_line, 'Blame Line' },
-                            B = { gitsigns.toggle_current_line_blame, 'Toggle Blame' },
-                            d = { gitsigns.diffthis, 'Diff This' },
-                            D = { function() gitsigns.diffthis('~') end, 'Diff Against HEAD' },
-                            t = { gitsigns.toggle_deleted, 'Toggle Deleted' },
-                        },
-                    }, { prefix = '<leader>', buffer = bufnr })
-                end,
-            })
-        end,
+                -- Hunk navigation (diff-aware)
+                map("n", "]c", function()
+                    if vim.wo.diff then vim.cmd.normal({ "]c", bang = false }) else gs.next_hunk() end
+                end, "Next Git Hunk")
+
+                map("n", "[c", function()
+                    if vim.wo.diff then vim.cmd.normal({ "[c", bang = false }) else gs.prev_hunk() end
+                end, "Prev Git Hunk")
+
+                -- Text objects for hunks (operator-pending/visual)
+                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Inner Hunk")
+                map({ "o", "x" }, "ah", ":<C-U>Gitsigns select_hunk<CR>", "Around Hunk")
+
+                -- which-key group under <leader>g (buffer-local)
+                local ok, wk = pcall(require, "which-key")
+                if ok then
+                    local add = wk.add or wk.register
+                    add({
+                        { "<leader>g",  group = "Git (gitsigns)" },
+
+                        -- works in NORMAL and VISUAL (selection becomes the range)
+                        { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>",  desc = "Stage Hunk",           mode = { "n", "v" } },
+                        { "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>",  desc = "Reset Hunk",           mode = { "n", "v" } },
+
+                        { "<leader>gS", gs.stage_buffer,                 desc = "Stage Buffer" },
+                        { "<leader>gu", gs.undo_stage_hunk,              desc = "Undo Stage Hunk" },
+                        { "<leader>gR", gs.reset_buffer,                 desc = "Reset Buffer" },
+
+                        { "<leader>gp", gs.preview_hunk,                 desc = "Preview Hunk (float)" },
+                        { "<leader>gP", gs.preview_hunk_inline,          desc = "Preview Hunk (inline)" },
+
+                        { "<leader>gb", gs.blame_line,                   desc = "Blame Line" },
+                        { "<leader>gB", gs.toggle_current_line_blame,    desc = "Toggle Line Blame" },
+
+                        { "<leader>gd", gs.diffthis,                     desc = "Diff This" },
+                        { "<leader>gD", function() gs.diffthis("~") end, desc = "Diff Against HEAD" },
+
+                        { "<leader>gt", gs.toggle_deleted,               desc = "Toggle Deleted Lines" },
+                        { "<leader>gx", gs.toggle_signs,                 desc = "Toggle Signs" },
+                    }, { buffer = bufnr })
+                end
+            end,
+        },
     },
 }
