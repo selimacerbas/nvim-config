3a67b68c2319ccca13db145c6f67c305acd994ba
taaacse4 <selim.acerbas@swisscom.com>
2025-09-02 21:17:11 +0200

    update nvim web icons

--- lua/custom/lazy-plugins/nvim-web-icons.lua
+++ lua/custom/lazy-plugins/nvim-web-icons.lua
@@ -1,15 +1,29 @@
 return {
-    -- nvim-web-devicons for filetype icons
     {
-        'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('nvim-web-devicons').setup({
-                -- Default options
-                override = {}, -- Override specific icons if needed
-                default = true -- Use default icons globally
-            })
-        end
-    },
+        "nvim-tree/nvim-web-devicons",
+        -- Keep it lazy; plugins like nvim-tree / bufferline will pull it in as a dependency.
+        lazy = true,
 
-    -- Add more plugins below as needed
+        opts = {
+            color_icons = true, -- colored glyphs
+            default     = true, -- fall back to a generic icon if no match
+            strict      = true, -- only use defined icons; unknowns use `default`
+
+            -- Uncomment & extend these to tailor specific files/extensions:
+            -- override_by_filename = {
+            --   ["Dockerfile"]        = { icon = "", color = "#2496ED", name = "Dockerfile" },
+            --   [".dockerignore"]     = { icon = "", color = "#2496ED", name = "DockerIgnore" },
+            --   ["Makefile"]          = { icon = "", color = "#6e9f18", name = "Makefile" },
+            --   ["LICENSE"]           = { icon = "󰿃", color = "#d0cfcc", name = "License" },
+            -- },
+            -- override_by_extension = {
+            --   mdx = { icon = "", color = "#519aba", name = "Mdx" },
+            --   tf  = { icon = "󱁢", color = "#7B42BC", name = "Terraform" },
+            -- },
+        },
+
+        config = function(_, opts)
+            require("nvim-web-devicons").setup(opts)
+        end,
+    },
 }
