d3ab763b95a1798f9e86d2b925f7e57260697ffb
taaacse4 <selim.acerbas@swisscom.com>
2025-10-04 17:24:05 +0200

    remove redundant keymaps

--- lua/custom/lazy-plugins/comment.lua
+++ lua/custom/lazy-plugins/comment.lua
@@ -1,85 +1,84 @@
 return {
-    {
-        "numToStr/Comment.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "folke/which-key.nvim" },
-        opts = function()
-            -- If ts-context-commentstring is installed, wire it automatically.
-            local ok, tscc = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-            return {
-                -- keep all defaults; only add pre_hook when available
-                padding = true,
-                sticky = true,
-                ignore = nil,
-                mappings = { basic = true, extra = true },
-                pre_hook = ok and tscc.create_pre_hook() or nil, -- enable tsx/jsx-aware comments when available
-            }
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "folke/which-key.nvim" },
+
+    -- which-key v3: register the <leader>c group early
+    init = function()
+      local ok, wk = pcall(require, "which-key")
+      if ok and wk.add then
+        wk.add({ { "<leader>c", group = "Comment" } })
+      end
+    end,
+
+    opts = function()
+      -- If ts-context-commentstring is installed, wire it automatically.
+      local ok, tscc = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      return {
+        -- keep all defaults; only add pre_hook when available
+        padding = true,
+        sticky  = true,
+        ignore  = nil,
+        mappings = { basic = true, extra = true },
+        pre_hook = ok and tscc.create_pre_hook() or nil, -- tsx/jsx-aware comments when available
+      }
+    end,
+
+    -- v3-friendly keys{} (normal & visual)
+    keys = {
+      -- NORMAL
+      {
+        "<leader>cc",
+        function() require("Comment.api").toggle.linewise.current() end,
+        mode = "n", silent = true, noremap = true, desc = "Comment: toggle line",
+      },
+      {
+        "<leader>cB",
+        function() require("Comment.api").toggle.blockwise.current() end,
+        mode = "n", silent = true, noremap = true, desc = "Comment: toggle block",
+      },
+
+      -- VISUAL (preserve selection, then operate)
+      {
+        "<leader>cc",
+        function()
+          local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+          vim.api.nvim_feedkeys(esc, "nx", false)
+          require("Comment.api").toggle.linewise(vim.fn.visualmode())
         end,
-
-        -- Lazy-load on first use of our leader helpers too
-        keys = {
-            -- NORMAL
-            {
-                "<leader>cc",
-                function() require("Comment.api").toggle.linewise.current() end,
-                mode = "n",
-                desc = "Comment: toggle line"
-            },
-            {
-                "<leader>cB",
-                function() require("Comment.api").toggle.blockwise.current() end,
-                mode = "n",
-                desc = "Comment: toggle block"
-            },
-
-            -- VISUAL (preserve selection, then operate)
-            {
-                "<leader>cc",
-                function()
-                    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-                    vim.api.nvim_feedkeys(esc, "nx", false)
-                    require("Comment.api").toggle.linewise(vim.fn.visualmode())
-                end,
-                mode = "v",
-                desc = "Comment: toggle line (visual)"
-            },
-
-            {
-                "<leader>cB",
-                function()
-                    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-                    vim.api.nvim_feedkeys(esc, "nx", false)
-                    require("Comment.api").toggle.blockwise(vim.fn.visualmode())
-                end,
-                mode = "v",
-                desc = "Comment: toggle block (visual)"
-            },
-        },
-
-        config = function(_, opts)
-            require("Comment").setup(opts) -- sets up defaults like gcc/gbc/gc/gb/gcO/gco/gcA :contentReference[oaicite:2]{index=2}
-
-            -- which-key discoverability without overriding native mappings
-            local ok, wk = pcall(require, "which-key")
-            if ok then
-                local add = wk.add or wk.register
-
-                -- Leader group (helpers we provided)
-                -- add({ { "<leader>c", group = "Comment" } }, { mode = "n" })
-
-                -- Show native Comment.nvim maps under the normal `g` prefix
-                add({
-                    { "gc", group = "Linewise comment" },
-                    { "gb", group = "Blockwise comment" },
-                }, { mode = { "n", "v" }, prefix = "g" })
-
-                -- Extra one-keystroke hints after `gc` (these already exist; we’re just labeling)
-                add({
-                    { "A", desc = "Comment end of line (gcA)" },
-                    { "o", desc = "Comment below (gco)" },
-                    { "O", desc = "Comment above (gcO)" },
-                }, { mode = "n", prefix = "gc" })
-            end
+        mode = "v", silent = true, noremap = true, desc = "Comment: toggle line (visual)",
+      },
+      {
+        "<leader>cB",
+        function()
+          local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+          vim.api.nvim_feedkeys(esc, "nx", false)
+          require("Comment.api").toggle.blockwise(vim.fn.visualmode())
         end,
+        mode = "v", silent = true, noremap = true, desc = "Comment: toggle block (visual)",
+      },
     },
+
+    config = function(_, opts)
+      require("Comment").setup(opts) -- sets gcc/gbc/gc/gb/gcO/gco/gcA, etc.
+
+      -- which-key discoverability for native 'gc'/'gb' trees
+      local ok, wk = pcall(require, "which-key")
+      if ok and wk.add then
+        -- Show native Comment.nvim maps under the normal `g` prefix
+        wk.add({
+          { "gc", group = "Linewise comment" },
+          { "gb", group = "Blockwise comment" },
+        }, { mode = { "n", "v" }, prefix = "g" })
+
+        -- Extra hints after `gc` (labels only; mappings already provided by Comment.nvim)
+        wk.add({
+          { "A", desc = "Comment end of line (gcA)" },
+          { "o", desc = "Comment below (gco)" },
+          { "O", desc = "Comment above (gcO)" },
+        }, { mode = "n", prefix = "gc" })
+      end
+    end,
+  },
 }
--- lua/custom/lazy-plugins/flutter-tools-dart.lua
+++ lua/custom/lazy-plugins/flutter-tools-dart.lua
@@ -1,150 +1,153 @@
 return {
-    -- Flutter tools (manages Dart LSP + run/debug/devices/etc.)
-    {
-        "nvim-flutter/flutter-tools.nvim",
-        ft = { "dart" },
-        cmd = {
-            "FlutterRun", "FlutterDebug", "FlutterDevices", "FlutterEmulators", "FlutterReload", "FlutterRestart",
-            "FlutterQuit",
-            "FlutterAttach", "FlutterDetach", "FlutterOutlineToggle", "FlutterOutlineOpen", "FlutterDevTools",
-            "FlutterDevToolsActivate",
-            "FlutterCopyProfilerUrl", "FlutterLspRestart", "FlutterSuper", "FlutterReanalyze", "FlutterRename",
-            "FlutterLogClear", "FlutterLogToggle",
-        },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "stevearc/dressing.nvim",
-            "folke/which-key.nvim",
-            "nvim-telescope/telescope.nvim",
-            -- DAP stack
-            "mfussenegger/nvim-dap",
-            { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
-            "theHamsta/nvim-dap-virtual-text",
-        },
-        opts = function()
-            return {
-                -- tasteful UX extras
-                widget_guides = { enabled = true }, -- experimental but handy
-                closing_tags  = {                   -- annotate closing widgets
-                    enabled = true,
-                    highlight = "Comment",
-                    prefix = ">",
-                    priority = 10,
-                },
-                debugger      = {               -- integrates with nvim-dap
-                    enabled = true,
-                    exception_breakpoints = {}, -- keep default (none)
-                    evaluate_to_string_in_debug_views = true,
-                    -- You can also add register_configurations here if you want custom flavors/targets later
-                },
-                -- you can enable these later if you like:
-                -- dev_log = { enabled = true },
-                -- outline = { auto_open = false },
-            }
-        end,
-        config = function(_, opts)
-            require("flutter-tools").setup(opts)
-
-            -- Telescope extension
-            pcall(function() require("telescope").load_extension("flutter") end)
-
-            -- DAP UI & virtual text
-            local dap_ok, dap = pcall(require, "dap")
-            if dap_ok then
-                local dapui = require("dapui")
-                require("dapui").setup()
-                require("nvim-dap-virtual-text").setup() -- inline values
-
-                -- auto-open/close UI on session start/stop
-                dap.listeners.after.event_initialized["dapui_flutter"] = function() dapui.open() end
-                dap.listeners.before.event_terminated["dapui_flutter"] = function() dapui.close() end
-                dap.listeners.before.event_exited["dapui_flutter"]     = function() dapui.close() end
-            end
-
-            -- which-key: Flutter group (keeps your previous layout)
-            local ok, wk = pcall(require, "which-key")
-            if ok then
-                local add = wk.add or wk.register
-                add({
-                    -- { "<leader>f",  group = "Flutter" },
-                    { "<leader>fr", "<cmd>FlutterRun<CR>",              desc = "Run Project" },
-                    { "<leader>fd", "<cmd>FlutterDebug<CR>",            desc = "Debug Run (DAP)" },
-                    { "<leader>fv", "<cmd>FlutterDevices<CR>",          desc = "Select Device" },
-                    { "<leader>fe", "<cmd>FlutterEmulators<CR>",        desc = "Select Emulator" },
-                    { "<leader>fl", "<cmd>FlutterReload<CR>",           desc = "Hot Reload" },
-                    { "<leader>fs", "<cmd>FlutterRestart<CR>",          desc = "Hot Restart" },
-                    { "<leader>fq", "<cmd>FlutterQuit<CR>",             desc = "Quit App" },
-                    { "<leader>fa", "<cmd>FlutterAttach<CR>",           desc = "Attach to App" },
-                    { "<leader>fx", "<cmd>FlutterDetach<CR>",           desc = "Detach Session" },
-                    { "<leader>fo", "<cmd>FlutterOutlineToggle<CR>",    desc = "Toggle Outline" },
-                    { "<leader>fO", "<cmd>FlutterOutlineOpen<CR>",      desc = "Open Outline" },
-                    { "<leader>ft", "<cmd>FlutterDevTools<CR>",         desc = "Start DevTools" },
-                    { "<leader>fT", "<cmd>FlutterDevToolsActivate<CR>", desc = "Activate DevTools" },
-                    { "<leader>fc", "<cmd>FlutterCopyProfilerUrl<CR>",  desc = "Copy Profiler URL" },
-                    { "<leader>fL", "<cmd>FlutterLspRestart<CR>",       desc = "Restart Dart LSP" },
-                    { "<leader>fS", "<cmd>FlutterSuper<CR>",            desc = "Go to Super" },
-                    { "<leader>fn", "<cmd>FlutterRename<CR>",           desc = "Rename & Update Imports" },
-                    { "<leader>fu", "<cmd>FlutterReanalyze<CR>",        desc = "Force Reanalyze" },
-                    { "<leader>fC", "<cmd>FlutterLogClear<CR>",         desc = "Clear Logs" },
-                    { "<leader>fG", "<cmd>FlutterLogToggle<CR>",        desc = "Toggle Logs" },
-                    -- Telescope helpers
-                    {
-                        "<leader>ff",
-                        function()
-                            local ok_t = pcall(require, "telescope")
-                            if ok_t then require("telescope").extensions.flutter.commands() end
-                        end,
-                        desc = "Telescope: Flutter Commands"
-                    },
-                    {
-                        "<leader>fF",
-                        function()
-                            local ok_t = pcall(require, "telescope")
-                            if ok_t and require("telescope").extensions.flutter.fvm then
-                                require("telescope").extensions.flutter.fvm()
-                            else
-                                vim.notify("FVM picker unavailable (enable fvm in flutter-tools)", vim.log.levels.INFO)
-                            end
-                        end,
-                        desc = "Telescope: Flutter FVM"
-                    },
-                }, { mode = "n", silent = true, noremap = true })
-            end
-        end,
+  -- Flutter tools (manages Dart LSP + run/debug/devices/etc.)
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    ft = { "dart" },
+    cmd = {
+      "FlutterRun", "FlutterDebug", "FlutterDevices", "FlutterEmulators", "FlutterReload", "FlutterRestart",
+      "FlutterQuit", "FlutterAttach", "FlutterDetach", "FlutterOutlineToggle", "FlutterOutlineOpen", "FlutterDevTools",
+      "FlutterDevToolsActivate", "FlutterCopyProfilerUrl", "FlutterLspRestart", "FlutterSuper", "FlutterReanalyze",
+      "FlutterRename", "FlutterLogClear", "FlutterLogToggle",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+      "folke/which-key.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- DAP stack
+      "mfussenegger/nvim-dap",
+      { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
+      "theHamsta/nvim-dap-virtual-text",
     },
 
-    -- Flutter BLoC boilerplate generator (code actions + commands)
-    {
-        "wa11breaker/flutter-bloc.nvim",
-        ft = { "dart" },
-        dependencies = { "nvimtools/none-ls.nvim" }, -- required for code actions
-        opts = {
-            bloc_type = "equatable",                 -- 'default' | 'equatable' | 'freezed'
-            use_sealed_classes = false,
-            enable_code_actions = true,
-        },
-        config = function(_, opts)
-            require("flutter-bloc").setup(opts)
-            -- Optional quick creators under <leader>fB… (kept inside Flutter group)
-            local ok, wk = pcall(require, "which-key")
-            if ok then
-                local add = wk.add or wk.register
-                add({
-                    { "<leader>fB",  group = "BLoC" },
-                    { "<leader>fBb", function() require("flutter-bloc").create_bloc() end,  desc = "Create Bloc" },
-                    { "<leader>fBc", function() require("flutter-bloc").create_cubit() end, desc = "Create Cubit" },
-                })
-            end
-        end,
+    -- which-key v3: register groups early
+    init = function()
+      local ok, wk = pcall(require, "which-key")
+      if ok and wk.add then
+        wk.add({
+          { "<leader>f",  group = "Flutter" },
+          { "<leader>fB", group = "BLoC" }, -- nested group for the bloc helper plugin
+        })
+      end
+    end,
+
+    -- v3-friendly keymaps (no sizing here—your global UI sizing handles that)
+    keys = {
+      { "<leader>fr", "<cmd>FlutterRun<CR>",               desc = "Run Project",                mode = "n", silent = true, noremap = true },
+      { "<leader>fd", "<cmd>FlutterDebug<CR>",             desc = "Debug Run (DAP)",            mode = "n", silent = true, noremap = true },
+      { "<leader>fv", "<cmd>FlutterDevices<CR>",           desc = "Select Device",              mode = "n", silent = true, noremap = true },
+      { "<leader>fe", "<cmd>FlutterEmulators<CR>",         desc = "Select Emulator",            mode = "n", silent = true, noremap = true },
+      { "<leader>fl", "<cmd>FlutterReload<CR>",            desc = "Hot Reload",                 mode = "n", silent = true, noremap = true },
+      { "<leader>fs", "<cmd>FlutterRestart<CR>",           desc = "Hot Restart",                mode = "n", silent = true, noremap = true },
+      { "<leader>fq", "<cmd>FlutterQuit<CR>",              desc = "Quit App",                   mode = "n", silent = true, noremap = true },
+      { "<leader>fa", "<cmd>FlutterAttach<CR>",            desc = "Attach to App",              mode = "n", silent = true, noremap = true },
+      { "<leader>fx", "<cmd>FlutterDetach<CR>",            desc = "Detach Session",             mode = "n", silent = true, noremap = true },
+      { "<leader>fo", "<cmd>FlutterOutlineToggle<CR>",     desc = "Toggle Outline",             mode = "n", silent = true, noremap = true },
+      { "<leader>fO", "<cmd>FlutterOutlineOpen<CR>",       desc = "Open Outline",               mode = "n", silent = true, noremap = true },
+      { "<leader>ft", "<cmd>FlutterDevTools<CR>",          desc = "Start DevTools",             mode = "n", silent = true, noremap = true },
+      { "<leader>fT", "<cmd>FlutterDevToolsActivate<CR>",  desc = "Activate DevTools",          mode = "n", silent = true, noremap = true },
+      { "<leader>fc", "<cmd>FlutterCopyProfilerUrl<CR>",   desc = "Copy Profiler URL",          mode = "n", silent = true, noremap = true },
+      { "<leader>fL", "<cmd>FlutterLspRestart<CR>",        desc = "Restart Dart LSP",           mode = "n", silent = true, noremap = true },
+      { "<leader>fS", "<cmd>FlutterSuper<CR>",             desc = "Go to Super",                mode = "n", silent = true, noremap = true },
+      { "<leader>fn", "<cmd>FlutterRename<CR>",            desc = "Rename & Update Imports",    mode = "n", silent = true, noremap = true },
+      { "<leader>fu", "<cmd>FlutterReanalyze<CR>",         desc = "Force Reanalyze",            mode = "n", silent = true, noremap = true },
+      { "<leader>fC", "<cmd>FlutterLogClear<CR>",          desc = "Clear Logs",                 mode = "n", silent = true, noremap = true },
+      { "<leader>fG", "<cmd>FlutterLogToggle<CR>",         desc = "Toggle Logs",                mode = "n", silent = true, noremap = true },
+
+      -- Telescope helpers
+      { "<leader>ff", function()
+          local ok_t = pcall(require, "telescope")
+          if ok_t then require("telescope").extensions.flutter.commands() end
+        end, desc = "Telescope: Flutter Commands", mode = "n", silent = true, noremap = true },
+
+      { "<leader>fF", function()
+          local ok_t = pcall(require, "telescope")
+          if ok_t and require("telescope").extensions.flutter.fvm then
+            require("telescope").extensions.flutter.fvm()
+          else
+            vim.notify("FVM picker unavailable (enable fvm in flutter-tools)", vim.log.levels.INFO)
+          end
+        end, desc = "Telescope: Flutter FVM", mode = "n", silent = true, noremap = true },
     },
 
-    -- Dart data-class generator (code actions via none-ls)
-    {
-        "wa11breaker/dart-data-class-generator.nvim",
-        ft = { "dart" },
-        dependencies = { "nvimtools/none-ls.nvim" }, -- provides the code actions
-        config = function()
-            require("dart-data-class-generator").setup({})
-        end,
+    opts = function()
+      return {
+        -- tasteful UX extras
+        widget_guides = { enabled = true }, -- experimental but handy
+        closing_tags  = {
+          enabled = true,
+          highlight = "Comment",
+          prefix = ">",
+          priority = 10,
+        },
+        debugger = { -- integrates with nvim-dap
+          enabled = true,
+          exception_breakpoints = {},
+          evaluate_to_string_in_debug_views = true,
+        },
+        -- dev_log / outline can be enabled later if desired
+        -- dev_log = { enabled = true },
+        -- outline = { auto_open = false },
+      }
+    end,
+
+    config = function(_, opts)
+      require("flutter-tools").setup(opts)
+
+      -- Telescope extension
+      pcall(function() require("telescope").load_extension("flutter") end)
+
+      -- DAP UI & virtual text
+      local dap_ok, dap = pcall(require, "dap")
+      if dap_ok then
+        local dapui = require("dapui")
+        dapui.setup()
+        require("nvim-dap-virtual-text").setup()
+
+        dap.listeners.after.event_initialized["dapui_flutter"] = function() dapui.open() end
+        dap.listeners.before.event_terminated["dapui_flutter"] = function() dapui.close() end
+        dap.listeners.before.event_exited["dapui_flutter"]     = function() dapui.close() end
+      end
+    end,
+  },
+
+  -- Flutter BLoC boilerplate generator (code actions + commands)
+  {
+    "wa11breaker/flutter-bloc.nvim",
+    ft = { "dart" },
+    dependencies = { "nvimtools/none-ls.nvim" }, -- required for code actions
+
+    init = function()
+      local ok, wk = pcall(require, "which-key")
+      if ok and wk.add then
+        wk.add({ { "<leader>fB", group = "BLoC" } })
+      end
+    end,
+
+    keys = {
+      { "<leader>fBb", function() require("flutter-bloc").create_bloc() end,
+        desc = "Create Bloc", mode = "n", silent = true, noremap = true },
+      { "<leader>fBc", function() require("flutter-bloc").create_cubit() end,
+        desc = "Create Cubit", mode = "n", silent = true, noremap = true },
     },
+
+    opts = {
+      bloc_type = "equatable",   -- 'default' | 'equatable' | 'freezed'
+      use_sealed_classes = false,
+      enable_code_actions = true,
+    },
+    config = function(_, opts)
+      require("flutter-bloc").setup(opts)
+    end,
+  },
+
+  -- Dart data-class generator (code actions via none-ls)
+  {
+    "wa11breaker/dart-data-class-generator.nvim",
+    ft = { "dart" },
+    dependencies = { "nvimtools/none-ls.nvim" }, -- provides the code actions
+    config = function()
+      require("dart-data-class-generator").setup({})
+    end,
+  },
 }
